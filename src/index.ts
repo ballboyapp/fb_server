fd51f378cdd7ded8d7c9726e0cf39bf28b03e018
fede-rodes <federodes@gmail.com>
2020-08-04 09:51:25 +0200

    auth middleware being fired

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express, { Express } from 'express';
 import admin from 'firebase-admin';
 import { https } from 'firebase-functions';
 import cors from 'cors';
-// import cookieParser from 'cookie-parser';
 import { authMiddleware } from './middlewares'
 import { gqlServer } from './graphql/server';
 
@@ -12,8 +11,7 @@ const app: Express = express();
 const isProduction = app.get('env') === 'production';
 console.log({ isProduction });
 
-app.use(cors);
-// app.use(cookieParser);
+app.use(cors()); // TODO
 app.use(authMiddleware);
 
 gqlServer(app);
